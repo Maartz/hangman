cce8306d9208d5cf1345c5a714e91d988003c4c8
Maartz <maartz@protonmail.com>
2019-10-20 21:18:39 +0200

    feat: add accept_move func and add tests

--- lib/hangman/game.ex
+++ lib/hangman/game.ex
@@ -3,7 +3,8 @@ defmodule Hangman.Game do
   defstruct(
     turns_left: 7,
     game_state: :initializing,
-    letters: []
+    letters: [],
+    used: MapSet.new()
   )
 
   def new_game() do
@@ -13,11 +14,22 @@ defmodule Hangman.Game do
   end
 
   # Make move with guard close btw lost or won
+  @spec make_move(map, any) :: {map, 12456}
   def make_move(game = %{game_state: state}, _guess) when state in [:won, :lost] do
     {game, tally(game)}
   end
 
   def make_move(game, guess) do
+    game = accept_move(game, guess, MapSet.member?(game.used, guess))
+    {game, tally(game)}
+  end
+
+  def accept_move(game, guess, _already_guess = true) do
+    Map.put(game, :game_state, :already_used)
+  end
+
+  def accept_move(game, guess, _already_guess) do
+    Map.put(game, :used, MapSet.put(game.used, guess))
   end
 
   def tally(game) do

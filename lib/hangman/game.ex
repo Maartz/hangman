995580e968176fe4382977d8ef6200f51ab286c4
Maartz <maartz@protonmail.com>
2019-10-20 21:01:42 +0200

    feat: add make_move func with tests

--- lib/hangman/game.ex
+++ lib/hangman/game.ex
@@ -11,4 +11,16 @@ defmodule Hangman.Game do
       letters: Dictionnary.random_words() |> String.codepoints()
     }
   end
+
+  # Make move with guard close btw lost or won
+  def make_move(game = %{game_state: state}, _guess) when state in [:won, :lost] do
+    {game, tally(game)}
+  end
+
+  def make_move(game, guess) do
+  end
+
+  def tally(game) do
+    12456
+  end
 end
--- test/game_test.exs
+++ test/game_test.exs
@@ -9,5 +9,14 @@ defmodule GameTest do
     assert game.turns_left == 7
     assert game.game_state == :initializing
     assert length(game.letters) > 0
+
+    assert List.ascii_printable?(game.letters, 0) == true
+  end
+
+  test "state isn't changed for :won or :lost game" do
+    for state <- [:won, :lost] do
+      game = Game.new_game() |> Map.put(:game_state, state)
+      assert {^game, _} = Game.make_move(game, "x")
+    end
   end
 end
